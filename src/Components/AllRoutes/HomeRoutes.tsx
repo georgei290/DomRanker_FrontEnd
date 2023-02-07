c94c9cba781e74f13a3e15c58aa1a964ed85392a
AnyamahNdidi <theo4felix@gmail.com>
2023-02-07 08:03:25 -0800

    auth ui

--- src/Components/AllRoutes/HomeRoutes.tsx
+++ src/Components/AllRoutes/HomeRoutes.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useRoutes } from "react-router";
+import ForgetPass from "../Auth/ForgetPass";
+import ResetPass from "../Auth/ResetPass";
+import SignIn from "../Auth/SignIn";
+import Signup from "../Auth/Signup";
 import Homescreen from "../HomeScreen";
 
 const HomeRoutes = () => {
@@ -7,6 +11,22 @@ const HomeRoutes = () => {
 		{
 			path: "/",
 			element: <Homescreen />,
+		},
+		{
+			path: "/signup",
+			element: <Signup />,
+		},
+		{
+			path: "/signin",
+			element: <SignIn />,
+		},
+		{
+			path: "/forget",
+			element: <ForgetPass />,
+		},
+		{
+			path: "/reset",
+			element: <ResetPass />,
 		},
 	]);
 	return element;
