767a1c10fb5dd24354355cbfc9a755e20bc17bb3
sammyalwayscode <olorundasamuel2@gmail.com>
2023-02-21 09:12:40 +0100

    keyword

--- src/Components/DashBoard/Keywords/KeyWords.tsx
+++ src/Components/DashBoard/Keywords/KeyWords.tsx
@@ -1,43 +1,44 @@
 import React from "react";
 import styled from "styled-components";
+import EmptyKeyWord from "./EmptyKeyWord";
 
 const KeyWords = () => {
-	return (
-		<Container>
-			<Wrapper>
-				<Title>Keywords</Title>
-			</Wrapper>
-		</Container>
-	);
+  return (
+    <Container>
+      <Wrapper>
+        <EmptyKeyWord />
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default KeyWords;
 
 const Title = styled.div`
-	color: #3e4581;
-	font-size: 25px;
-	font-weight: 800;
+  color: #3e4581;
+  font-size: 25px;
+  font-weight: 800;
 `;
 
 const Wrapper = styled.div`
-	margin-left: 30px;
-	margin-top: 30px;
+  margin-left: 20px;
+  margin-top: 10px;
 `;
 
 const Container = styled.div`
-	min-width: calc(100vw - 240px);
-	min-height: calc(100vh - 60px);
-	display: flex;
-	overflow: hidden;
+  min-width: calc(100vw - 240px);
+  min-height: calc(100vh - 60px);
+  display: flex;
+  overflow: hidden;
 
-	/* justify-content: space-between; */
-	/* align-items: center; */
-	flex-direction: column;
-	background-color: #f8f8ff;
-	margin-top: 60px;
+  /* justify-content: space-between; */
+  /* align-items: center; */
+  flex-direction: column;
+  background-color: #f8f8ff;
+  margin-top: 60px;
 
-	@media screen and (max-width: 768px) {
-		width: 100%;
-	}
-	/* flex-direction: column; */
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+  /* flex-direction: column; */
 `;
