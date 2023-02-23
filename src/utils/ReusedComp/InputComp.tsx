66892147874fa027f6e98f75144006f2c84a4e19
sammyalwayscode <olorundasamuel2@gmail.com>
2023-02-23 15:40:06 +0100

    sammy

--- src/utils/ReusedComp/InputComp.tsx
+++ src/utils/ReusedComp/InputComp.tsx
@@ -43,7 +43,7 @@ const InputComp = () => {
       <Main>
         <InputText>Search Engine Types</InputText>
         <Input2>
-          <Input3 placeholder="Enter search" />
+          <Input3 placeholder="Enter search" type="search" />
 
           {
             // Google
@@ -237,13 +237,14 @@ const Input3 = styled.input`
   height: 100%;
   outline: none;
   border: none;
+  font-family: Montserrat;
 `;
 const Input2 = styled.div`
-  height: 50px;
+  height: 35px;
   width: 98%;
   background-color: white;
-  border: 1px solid #f1f1f1;
-  border-radius: 5px;
+  border: 1px solid #e2e2e2;
+  border-radius: 2px;
   display: flex;
   align-items: center;
   overflow: hidden;
@@ -252,23 +253,25 @@ const Input2 = styled.div`
   }
 `;
 const Button = styled.button`
-  margin-right: 10px;
-  height: 40px;
+  margin-right: 3px;
+  height: 32px;
   width: 150px;
   color: white;
   border: none;
   outline: none;
-  border-radius: 10px;
+  border-radius: 2px;
   cursor: pointer;
   background-color: #ae67fa;
-  font-weight: bold;
+  font-weight: 600;
+  font-size: 12px;
 `;
 
 const Select = styled.select`
   width: 100%;
-  height: 40px;
-  border-radius: 5px;
-  border: 1px solid #f1f1f1;
+  height: 35px;
+  border-radius: 2px;
+  border: 1px solid #e2e2e2;
+  font-family: Montserrat;
   outline: none;
   padding-left: 10px;
   margin-right: 10px;
@@ -291,7 +294,7 @@ const InputHold = styled.div`
 `;
 const Main = styled.div`
   width: 100%;
-  margin-top: 15px;
+  margin-top: 10px;
   margin-right: 40px;
 `;
 const Input = styled.input`
@@ -312,8 +315,9 @@ const Input = styled.input`
   }
 `;
 const InputText = styled.div`
-  font-size: 13px;
-  margin-bottom: 6px;
+  font-size: 12px;
+  margin-bottom: 3px;
+  font-weight: 600;
   /* font-weight: 600; */
 `;
 
