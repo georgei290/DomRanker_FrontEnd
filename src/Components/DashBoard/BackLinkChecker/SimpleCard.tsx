4d4f09856e28a670a8b4b0b07ab312da44616241
georgei290 <georgeseo06@gmail.com>
2023-02-24 13:05:55 +0100

    arranged

--- src/Components/DashBoard/BackLinkChecker/SimpleCard.tsx
+++ src/Components/DashBoard/BackLinkChecker/SimpleCard.tsx
@@ -1,38 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 
-const SimpleCard = () => {
-  return (
-    <Main>
-      <Title>Backlinks</Title>
-      <MainTitle>6,842</MainTitle>
-    </Main>
-  );
+interface Iprops {
+	BackTitle: string;
+}
+
+const SimpleCard: React.FC<Iprops> = ({ BackTitle }) => {
+	return (
+		<Main>
+			<Title>{BackTitle}</Title>
+			<MainTitle>6,842</MainTitle>
+		</Main>
+	);
 };
 
 export default SimpleCard;
 
 const Title = styled.div`
-  font-weight: 600;
+	font-weight: 600;
 `;
 
 const MainTitle = styled.div`
-  font-weight: 800;
-  font-size: 35px;
-  margin-top: 8px;
+	font-weight: 800;
+	font-size: 35px;
+	margin-top: 8px;
 `;
 
 const Main = styled.div`
-  width: 290px;
-  height: 100px;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 10px;
+	width: 290px;
+	height: 100px;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin: 10px;
 
-  @media screen and (max-width: 320px) {
-    width: 95%;
-  }
+	@media screen and (max-width: 320px) {
+		width: 95%;
+	}
 `;
