7f78a719b50f39ecd8c1a19168694ec41eeb241b
sammyalwayscode <olorundasamuel2@gmail.com>
2023-02-25 19:54:13 +0100

    sammy

--- src/Components/DashBoard/OnPageData/FirstTable.tsx
+++ src/Components/DashBoard/OnPageData/FirstTable.tsx
@@ -1,125 +1,104 @@
-import React from 'react'
-import styled from 'styled-components'
-import {HashData} from "./FirstContent"
+import React from "react";
+import styled from "styled-components";
+import { HashData } from "./FirstContent";
 
 const FirstTable = () => {
   return (
     <>
-    
-					<HoldTable>
-						<Table>
-							<Headrow>
-								<TheadCT>
-									#
-								</TheadCT>
-								
-								<Thead>
-								htags[h1]
-								</Thead>
-						
-							</Headrow>
+      <HoldTable>
+        <Table>
+          <Headrow>
+            <TheadCT>#</TheadCT>
 
-                            {
-                                HashData.map((props)=>(
-                                    <ContentRow>
-                                    <ContentData>
-                                        <MainData1>
-                                            <a>{props.hashTags}</a>
-                                        </MainData1>
-                                    </ContentData>
-                                   
-                                    <ContentDataTV>
-                                        <MainData>
-                                        {props.con}
-                                        </MainData>
-                                    </ContentDataTV>
-                                   
-                                </ContentRow>
-                                ))
-                            }
-						
+            <Thead>htags[h1]</Thead>
+          </Headrow>
 
-						</Table>
-					</HoldTable>
-				
-     </>
-  )
-}
+          {HashData.map((props) => (
+            <ContentRow>
+              <ContentData>
+                <MainData1>
+                  <a>{props.hashTags}</a>
+                </MainData1>
+              </ContentData>
 
-export default FirstTable
+              <ContentDataTV>
+                <MainData>{props.con}</MainData>
+              </ContentDataTV>
+            </ContentRow>
+          ))}
+        </Table>
+      </HoldTable>
+    </>
+  );
+};
+
+export default FirstTable;
 
 const MainData1 = styled.div`
-width:100%;
-height:45px;
+  width: 100%;
+  height: 45px;
 
-display: flex;
-align-items: center;
-`
+  display: flex;
+  align-items: center;
+`;
 
 const MainData = styled.div`
-width:100%;
+  width: 100%;
 
-height:40px;
-display: flex;
-padding-left:10px;
-`
+  height: 40px;
+  display: flex;
+  padding-left: 10px;
+`;
 
 const TheadCT = styled.th`
-width:auto;
-height:50%;
-padding-left:10px;
-padding-right:20px;
-border-right:2px solid #E2E2E2;
-`
+  width: auto;
+  height: 50%;
+  padding-left: 10px;
+  padding-right: 20px;
+  border-right: 2px solid #e2e2e2;
+`;
 const TheadL = styled.th`
-padding-left:10px;
-padding-right:848px;
-border-right:2px solid #E2E2E2;
-`
+  padding-left: 10px;
+  padding-right: 848px;
+  border-right: 2px solid #e2e2e2;
+`;
 const Thead = styled.th`
-padding-left:10px;
-padding-right:95%;
+  padding-left: 10px;
+  padding-right: 95%;
+`;
 
-`
-
-const ContentDataTV  = styled.td`
-width:850px;
-height:45px;
-
-
-
-
-`
-const ContentData  = styled.td`
-width:20px;
-height:45px;
-border-right:2px solid #E2E2E2;
-padding-left:10px;
-`
-const ContentRow  = styled.tr`
- flex: 1;
-:nth-child(even) {
-    background-color: #FAFAFC;
+const ContentDataTV = styled.td`
+  width: 850px;
+  height: 45px;
+`;
+const ContentData = styled.td`
+  width: 20px;
+  height: 45px;
+  border-right: 2px solid #e2e2e2;
+  padding-left: 10px;
+`;
+const ContentRow = styled.tr`
+  flex: 1;
+  :nth-child(even) {
+    background-color: #fafafc;
   }
-`
+`;
 
 const Headrow = styled.tr`
-height:40px;
-
-`
+  height: 40px;
+`;
 
 const Table = styled.table`
-width: 100%;
+  width: 100%;
 
-@media screen and (max-width: 768px) {
-    width:auto;
-}
-`
+  @media screen and (max-width: 768px) {
+    width: auto;
+  }
+`;
 const HoldTable = styled.div`
-overflow-x: auto;
+  overflow-x: auto;
 
-@media screen and (max-width:768px){
-    width:100%;
-}
-`
-
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
